a8286e05bcb1b66454f43f175e66d63cf6ce7267
Emile <estudio.123455@gmail.com>
2025-10-11 23:28:50 -0500

    Fix ECDSA error: add NEXTAUTH_SECRET to auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,7 @@ import { prisma } from './prisma'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     ...(process.env.GOOGLE_CLIENT_ID
       ? [
